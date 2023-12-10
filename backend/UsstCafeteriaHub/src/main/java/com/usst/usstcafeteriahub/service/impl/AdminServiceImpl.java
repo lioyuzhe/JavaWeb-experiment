c26a84472012879712c099ad99114fe25fe34388
Morritte Klein <Kleinliojie@outlook.com>
2023-12-10 14:47:14 +0800

    Admin 和 Cafeteria表  李英杰

--- backend/UsstCafeteriaHub/src/main/java/com/usst/usstcafeteriahub/service/impl/AdminServiceImpl.java
+++ backend/UsstCafeteriaHub/src/main/java/com/usst/usstcafeteriahub/service/impl/AdminServiceImpl.java
@@ -1,19 +1,19 @@
 package com.usst.usstcafeteriahub.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.usst.usstcafeteriahub.model.entity.Admin;
 import com.usst.usstcafeteriahub.service.AdminService;
 import com.usst.usstcafeteriahub.mapper.AdminMapper;
-import com.usst.usstcafeteriahub.model.entity.Admin;
 import org.springframework.stereotype.Service;
 
 /**
 * @author Klein
 * @description 针对表【admin(系统管理员表)】的数据库操作Service实现
-* @createDate 2023-12-06 22:04:37
+* @createDate 2023-12-10 14:39:20
 */
 @Service
 public class AdminServiceImpl extends ServiceImpl<AdminMapper, Admin>
-    implements AdminService {
+    implements AdminService{
 
 }
 
