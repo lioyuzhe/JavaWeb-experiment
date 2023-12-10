a806575569df93497f638d669fe2d52a61db9bf2
Sfangfei <2577407055@qq.com>
2023-12-10 17:04:00 +0800

    首页+投票 孙芳菲

--- frontend/UsstCafeteriaHub/src/views/ts/ts_home.vue
+++ frontend/UsstCafeteriaHub/src/views/ts/ts_home.vue
@@ -1,11 +1,62 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 
+// 示例数据
+const promotions = ref([]);
+const polls = ref([]);
+const foodRecommendations = ref([]);
+const communityTopics = ref([]);
+const notifications = ref([]);
+
+// 在组件挂载时获取数据
+onMounted(() => {
+  // 获取数据的方法
+  // fetchPromotions();
+  // fetchPolls();
+  // ...
+});
 </script>
 
-<template>
 
+<template>
+  <div class="home">
+    <section class="promotions">
+      <h2>最新促销信息</h2>
+      <!-- 促销信息列表 -->
+    </section>
+
+    <section class="polls">
+      <h2>最新投票调查</h2>
+      <!-- 投票调查列表 -->
+    </section>
+
+    <section class="food-recommendations">
+      <h2>食堂推荐菜品</h2>
+      <!-- 推荐菜品列表 -->
+    </section>
+
+    <section class="community-topics">
+      <h2>社区热门话题</h2>
+      <!-- 热门话题列表 -->
+    </section>
+
+    <!-- 更多板块... -->
+
+    <section class="notifications">
+      <h2>通知</h2>
+      <!-- 通知列表 -->
+    </section>
+  </div>
 </template>
 
 <style scoped>
+.home {
+  /* 首页样式 */
+}
 
+.home section {
+  margin-bottom: 20px;
+}
+
+/* 更多样式定义... */
 </style>
