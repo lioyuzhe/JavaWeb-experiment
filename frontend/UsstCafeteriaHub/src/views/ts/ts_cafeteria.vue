eacd65bb2d0ba58d6d343197805240bed96507ad
钳砸 <1029728179@qq.com>
2023-12-11 03:39:28 +0800

    更新了食堂顶部内容和侧边栏列表

--- frontend/UsstCafeteriaHub/src/views/ts/ts_cafeteria.vue
+++ frontend/UsstCafeteriaHub/src/views/ts/ts_cafeteria.vue
@@ -1,42 +1,38 @@
 <template>
-  <div>
-    <!-- 父组件 -->
-    <div class="parent-component">
-      <!-- 内容 -->
-    </div>
-    <!-- 顶部搜索和食堂选择区域 -->
-    <div class="top-container">
-      <!-- 食堂搜索框 -->
-      <el-input
-          class="cafeteria-search"
-          placeholder="搜索食堂"
-          prefix-icon="el-icon-search">
-      </el-input>
-      <!-- 食堂选择列表 -->
-      <div class="cafeteria-select">
-        <el-button style="width: 100px;">一食堂</el-button>
-        <el-button style="width: 100px;">二食堂</el-button>
-        <el-button style="width: 100px;">三食堂</el-button>
-        <el-button style="width: 100px;">四食堂</el-button>
-        <!-- 更多按钮 -->
-        <el-button class="more-cafeterias" icon="el-icon-arrow-right"></el-button>
+  <div class="main-container">
+    <!-- 侧边栏 -->
+    <el-menu class="sidebar" style="width: 20%;" default-active="1">
+      <el-menu-item index="1">食堂介绍</el-menu-item>
+      <el-menu-item index="2">食堂菜品</el-menu-item>
+      <el-menu-item index="3">投票调查</el-menu-item>
+      <el-menu-item index="4">投诉食堂</el-menu-item>
+    </el-menu>
+
+    <!-- 右侧内容区域 -->
+    <div class="content" style="width: 80%;">
+      <!-- 顶部搜索和食堂选择区域 -->
+      <div class="top-container">
+        <!-- 食堂搜索框 -->
+        <el-input
+            class="cafeteria-search"
+            placeholder="搜索食堂"
+            prefix-icon="el-icon-search">
+        </el-input>
+        <!-- 食堂选择列表 -->
+        <div class="cafeteria-select">
+          <el-button style="width: 100px;">一食堂</el-button>
+          <el-button style="width: 100px;">二食堂</el-button>
+          <el-button style="width: 100px;">三食堂</el-button>
+          <!-- 更多按钮 -->
+          <el-button class="more-cafeterias" icon="el-icon-arrow-right"></el-button>
+        </div>
+        <!-- 菜品搜索框 -->
+        <el-input
+            class="dish-search"
+            placeholder="搜索菜品"
+            prefix-icon="el-icon-search">
+        </el-input>
       </div>
-      <!-- 菜品搜索框 -->
-      <el-input
-          class="dish-search"
-          placeholder="搜索菜品"
-          prefix-icon="el-icon-search">
-      </el-input>
-    </div>
-    <!-- 侧边栏与内容区域 -->
-    <div class="main-layout">
-      <!-- 侧边栏 -->
-      <el-menu class="el-menu-vertical-demo" style="width: 100%;" default-active="1">
-        <el-menu-item index="1">食堂介绍</el-menu-item>
-        <el-menu-item index="2">菜品</el-menu-item>
-        <el-menu-item index="3">投票</el-menu-item>
-        <el-menu-item index="4">投诉</el-menu-item>
-      </el-menu>
       <!-- 内容区域 -->
       <div class="content-area">
         <!-- 内容区域的子组件或元素 -->
@@ -76,6 +72,10 @@ export default {
 
 <style scoped>
 
+.main-container {
+  display: flex;
+}
+
 .top-container {
   display: flex;
   align-items: center;
@@ -83,7 +83,7 @@ export default {
 }
 
 .cafeteria-search {
-  flex: 0 1 50%; /* 设置基础宽度为50%，并允许收缩但不伸展 */
+  flex: 0 1 50%;
   margin-right: 10px;
 }
 
@@ -98,11 +98,6 @@ export default {
 
 .dish-search {
   margin-left: auto; /* 自动占据所有剩余空间，将元素推到最右边 */
-}
-
-.main-layout {
-  display: flex;
-  flex-grow: 1;
 }
 
 .content-area {
