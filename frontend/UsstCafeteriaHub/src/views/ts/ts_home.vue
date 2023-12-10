cec33c1129a256ca67afe9907a7232a6169033e0
Sfangfei <2577407055@qq.com>
2023-12-10 20:51:59 +0800

    1

--- frontend/UsstCafeteriaHub/src/views/ts/ts_home.vue
+++ frontend/UsstCafeteriaHub/src/views/ts/ts_home.vue
@@ -1,62 +1,90 @@
-<script setup>
-import { ref, onMounted } from 'vue';
-
-// 示例数据
-const promotions = ref([]);
-const polls = ref([]);
-const foodRecommendations = ref([]);
-const communityTopics = ref([]);
-const notifications = ref([]);
-
-// 在组件挂载时获取数据
-onMounted(() => {
-  // 获取数据的方法
-  // fetchPromotions();
-  // fetchPolls();
-  // ...
-});
-</script>
-
-
 <template>
-  <div class="home">
-    <section class="promotions">
-      <h2>最新促销信息</h2>
-      <!-- 促销信息列表 -->
-    </section>
-
-    <section class="polls">
-      <h2>最新投票调查</h2>
-      <!-- 投票调查列表 -->
-    </section>
-
-    <section class="food-recommendations">
-      <h2>食堂推荐菜品</h2>
-      <!-- 推荐菜品列表 -->
-    </section>
-
-    <section class="community-topics">
-      <h2>社区热门话题</h2>
-      <!-- 热门话题列表 -->
-    </section>
-
-    <!-- 更多板块... -->
-
-    <section class="notifications">
-      <h2>通知</h2>
-      <!-- 通知列表 -->
-    </section>
+  <div id="app">
+    <ul class="navigation">
+      <li><a href="#" @click="goto('home')">首页</a></li>
+      <li><a href="#" @click="goto('community')">社区</a></li>
+      <li><a href="#" @click="goto('canteen')">食堂</a></li>
+    </ul>
+    <div class="personal-information">
+      <div class="avatar-container" @click="goto('profile')">
+        <img src="avatar.png" alt="头像" class="avatar">
+        <div class="notification-dot" v-if="hasUnread"></div>
+      </div>
+    </div>
   </div>
 </template>
 
+<script setup>
+import { ref } from 'vue';
+
+const hasUnread = ref(true); // 假设有未读消息
+
+const goto = (page) => {
+  // 根据page参数跳转到不同页面
+  console.log('Navigate to:', page);
+  // 这里可以添加实际的导航逻辑
+};
+</script>
+
 <style scoped>
-.home {
-  /* 首页样式 */
+#app {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 14px 70px; /* 调整内边距以匹配140%缩放效果 */
+  background-color: #fff;
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.1);
 }
-
-.home section {
-  margin-bottom: 20px;
+.navigation {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
 }
-
-/* 更多样式定义... */
+.navigation li {
+  padding: 14px 42px; /* 增加间距 */
+  transition: background-color 0.3s;
+}
+.navigation li:not(:last-of-type) {
+  border-right: 1px solid #ddd;
+}
+.navigation li a {
+  text-decoration: none;
+  color: #333;
+  font-weight: 600;
+  font-size: 22px; /* 增加字体大小 */
+  transition: color 0.3s;
+}
+.navigation li a:hover, .navigation li a:focus {
+  color: #d32f2f;
+}
+.personal-information {
+  display: flex;
+  align-items: center;
+}
+.avatar-container {
+  position: relative;
+  padding-left: 28px; /* 调整头像容器的左填充 */
+  cursor: pointer;
+}
+.avatar {
+  width: 56px; /* 增加头像大小 */
+  height: 56px; /* 增加头像大小 */
+  border-radius: 50%;
+  border: 4px solid #d32f2f; /* 增加边框宽度 */
+  transition: border-color 0.3s;
+}
+.avatar:hover {
+  border-color: #ff6659;
+}
+.notification-dot {
+  position: absolute;
+  top: -2px;
+  right: 0;
+  width: 14px; /* 增加通知点大小 */
+  height: 14px; /* 增加通知点大小 */
+  background-color: #d32f2f;
+  border-radius: 50%;
+  border: 3px solid #fff; /* 增加边框宽度 */
+}
 </style>
