a0df15176ac2f1ad45db29c06b9610acb94873ae
Morritte Klein <Kleinliojie@outlook.com>
2023-12-15 15:08:52 +0800

    接口开发 李英杰

--- backend/UsstCafeteriaHub/src/main/java/com/usst/usstcafeteriahub/controller/CafeteriaAdminController.java
+++ backend/UsstCafeteriaHub/src/main/java/com/usst/usstcafeteriahub/controller/CafeteriaAdminController.java
@@ -49,6 +49,10 @@ public class CafeteriaAdminController {
     private CommunityMessageService communityMessageService;
     @Resource
     private CafeteriaNoticeService cafeteriaNoticeService;
+    @Resource
+    private ComplaintService complaintService;
+    @Resource
+    private PromotionService promotionService;
 
 
     @ApiOperation(value = "测试接口")
@@ -220,7 +224,7 @@ public class CafeteriaAdminController {
     // 食堂评价处理
     @ApiOperation("按照食堂ID筛选食堂评价")
     @GetMapping("/getCafeteriaRemarksByCafeteriaID")
-    public BaseResponse getCafeteriaRemarksByCafeteriaID(@RequestParam Integer id){
+    public BaseResponse getCafeteriaRemarksByCafeteriaID(@RequestParam Long id){
         if(id == null){
             return Result.error("参数为空");
         }
@@ -245,17 +249,208 @@ public class CafeteriaAdminController {
     }
 
 
-//    // 活动公告
-//    @ApiOperation("获取所有食堂活动公告")
-//    @GetMapping("/getAllCafeteriaNotice")
-//    public BaseResponse getAllCafeteriaNotice(){
-//        return Result.success();
-//    }
+    // 活动公告
+    @ApiOperation("添加食堂公告")
+    @PostMapping("/addCafeteriaNotice")
+    public BaseResponse addCafeteriaNotice(@RequestBody CafeteriaNotice cafeteriaNotice) {
+        if (cafeteriaNotice == null) {
+            return Result.error("参数为空");
+        }
+        log.info("添加食堂公告: {}", cafeteriaNotice);
+        boolean save = cafeteriaNoticeService.save(cafeteriaNotice);
+        if(!save){
+            return Result.error("添加失败");
+        }
+        return Result.success("添加成功");
+    }
+
+    @ApiOperation("按照食堂ID筛选食堂公告")
+    @GetMapping("/getCafeteriaNoticesByCafeteriaID")
+    public BaseResponse getCafeteriaNoticesByCafeteriaID(@RequestParam Long id){
+        if(id == null){
+            return Result.error("参数为空");
+        }
+        log.info("传入食堂id参数: {}", id);
+        List<CafeteriaNotice> list = cafeteriaNoticeService.getCafeteriaNoticesByCafeteriaID(id);
+        log.info("获取食堂公告列表: {}", list);
+        return Result.success(list);
+    }
+
+    @ApiOperation("删除食堂公告")
+    @PostMapping("/deleteCafeteriaNotice")
+    public BaseResponse deleteCafeteriaNotice(@RequestBody CafeteriaNotice cafeteriaNotice) {
+        if (cafeteriaNotice == null) {
+            return Result.error("参数为空");
+        }
+        log.info("要删除食堂公告: {}", cafeteriaNotice);
+        boolean remove = cafeteriaNoticeService.removeById(cafeteriaNotice.getNoticeId());
+        if (!remove) {
+            return Result.error("删除失败");
+        }
+        return Result.success("删除成功");
+    }
+
+    @ApiOperation("修改食堂公告")
+    @PostMapping("/updateCafeteriaNotice")
+    public BaseResponse updateCafeteriaNotice(@RequestBody CafeteriaNotice cafeteriaNotice) {
+        if (cafeteriaNotice == null) {
+            return Result.error("参数为空");
+        }
+        log.info("要修改食堂公告: {}", cafeteriaNotice);
+        boolean update = cafeteriaNoticeService.updateById(cafeteriaNotice);
+        if (!update) {
+            return Result.error("修改失败");
+        }
+        return Result.success("修改成功");
+    }
+
+    // 投诉信息管理
+    @ApiOperation("获取投诉列表")
+    @GetMapping("/getComplaint")
+    public BaseResponse getComplaint() {
+        List<Complaint> list = complaintService.list();
+        log.info("获取投诉列表: {}", list);
+        return Result.success(list);
+    }
+
+    @ApiOperation("添加投诉")
+    @PostMapping("/addComplaint")
+    public BaseResponse addComplaint(@RequestBody Complaint complaint) {
+        if (complaint == null) {
+            return Result.error("参数为空");
+        }
+        log.info("添加投诉: {}", complaint);
+        complaint.setStatus(0);
+        boolean save = complaintService.save(complaint);
+        if(!save){
+            return Result.error("添加失败");
+        }
+        return Result.success("添加成功");
+    }
+
+    @ApiOperation("删除投诉")
+    @PostMapping("/deleteComplaint")
+    public BaseResponse deleteComplaint(@RequestBody Complaint complaint) {
+        if (complaint == null) {
+            return Result.error("参数为空");
+        }
+        log.info("要删除投诉: {}", complaint);
+        boolean remove = complaintService.removeById(complaint.getComplaintId());
+        if (!remove) {
+            return Result.error("删除失败");
+        }
+        return Result.success("删除成功");
+    }
+
+    @ApiOperation("修改投诉")
+    @PostMapping("/updateComplaint")
+    public BaseResponse updateComplaint(@RequestBody Complaint complaint) {
+        if (complaint == null) {
+            return Result.error("参数为空");
+        }
+        log.info("修改投诉: {}", complaint);
+        boolean update = complaintService.updateById(complaint);
+        if(!update){
+            return Result.error("修改失败");
+        }
+        return Result.success("修改成功");
+    }
+
+    @ApiOperation("食堂管理员根据食堂id获取投诉列表,且未处理的投诉优先显示")
+    @GetMapping("/getComplaintByCafeteriaId")
+    public BaseResponse getComplaintByCafeteriaId(@RequestParam("cafeteriaId") Long cafeteriaId) {
+        if (cafeteriaId == null) {
+            return Result.error("参数为空");
+        }
+        List<Complaint> list = complaintService.getComplaintByCafeteriaId(cafeteriaId);
+        if (list == null) {
+            return Result.error("查询失败");
+        }
+        log.info("根据食堂id获取投诉列表: {}", list);
+        return Result.success(list);
+    }
+
+    @ApiOperation("食堂管理员根据食堂id获取投诉列表获取未处理的投诉数量")
+    @GetMapping("/getComplaintNumByCafeteriaId")
+    public BaseResponse getComplaintNumByCafeteriaId(@RequestParam("cafeteriaId") Long cafeteriaId) {
+        if (cafeteriaId == null) {
+            return Result.error("参数为空");
+        }
+        List<Complaint> list = complaintService.getComplaintByCafeteriaId(cafeteriaId);
+        int num = 0;
+        for (Complaint complaint : list) {
+            if (complaint.getStatus() == 0) {
+                num++;
+            }
+        }
+        log.info("根据食堂id获取投诉列表: {}", list);
+        return Result.success(num);
+    }
+
+    @ApiOperation("用户根据用户id获取已回复的投诉列表")
+    @GetMapping("/getComplaintReplyByUserId")
+    public BaseResponse getComplaintReplyByUserId(@RequestParam("userId") Long userId) {
+        if (userId == null) {
+            return Result.error("参数为空");
+        }
+        List<Complaint> list = complaintService.getComplaintReplyByUserId(userId);
+        if (list == null) {
+            return Result.error("查询失败");
+        }
+        log.info("根据用户id获取已回复的投诉列表: {}", list);
+        return Result.success(list);
+    }
 
 
+    @ApiOperation("获取促销菜品列表")
+    @GetMapping("/getPromotion")
+    public BaseResponse getPromotion() {
+        List<Promotion> list = promotionService.list();
+        log.info("获取促销菜品列表: {}", list);
+        return Result.success(list);
+    }
 
+    @ApiOperation("删除促销菜品")
+    @PostMapping("/deletePromotion")
+    public BaseResponse deletePromotion(@RequestBody Promotion promotion) {
+        if (promotion == null) {
+            return Result.error("参数为空");
+        }
+        log.info("要删除促销菜品: {}", promotion);
+        boolean remove = promotionService.removeById(promotion.getPromotionId());
+        if (!remove) {
+            return Result.error("删除失败");
+        }
+        return Result.success("删除成功");
+    }
 
+    @ApiOperation("添加促销菜品")
+    @PostMapping("/addPromotion")
+    public BaseResponse addPromotion(@RequestBody Promotion promotion) {
+        if (promotion == null) {
+            return Result.error("参数为空");
+        }
+        log.info("添加促销菜品: {}", promotion);
+        boolean save = promotionService.save(promotion);
+        if(!save){
+            return Result.error("添加失败");
+        }
+        return Result.success("添加成功");
+    }
 
+    @ApiOperation("修改促销菜品")
+    @PostMapping("/updatePromotion")
+    public BaseResponse updatePromotion(@RequestBody Promotion promotion) {
+        if (promotion == null) {
+            return Result.error("参数为空");
+        }
+        log.info("修改促销菜品: {}", promotion);
+        boolean update = promotionService.updateById(promotion);
+        if(!update){
+            return Result.error("修改失败");
+        }
+        return Result.success("修改成功");
+    }
 
 
 }
